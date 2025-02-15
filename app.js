775c9af3594400548d354411f06f682403cc2287
Ayush Raj <ar898993@gmail.com>
2025-02-15 13:32:38 +0530

    First git push and added frontend code

--- app.js
+++ app.js
@@ -1,41 +1,138 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const axios = require("axios");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const multer = require("multer");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const MongoStore = require("connect-mongo");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const fs = require("fs");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { isLoggedIn } = require("./middleware.js");
+const PORT = process.env.PORT;
 
-var app = express();
+const app = express();
+const dbUrl = process.env.ATLASDB_URL;
+const genAI = new GoogleGenerativeAI("AIzaSyAB7blS2dxS4vZk83B0f-CbhRpwfOgyiWM");
+app.locals.AppName = "WMS";
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Configure storage for uploaded files
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
-app.use(logger('dev'));
+// Configure session store
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: { secret: process.env.SECRET },
+  touchAfter: 24 * 60 * 60,
+});
+store.on("error", (error) => {
+  console.error("Error in MONGO SESSION STORE:", error);
+});
+
+// Session options
+const sessionOptions = {
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+// Middleware setup
+app.use(session(sessionOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Passport setup
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Middleware to make user data available in templates
+// app.use((req, res, next) => {
+//   res.locals.currUser = req.user;
+//   next();
+// });
+
+// Connect to the database
+async function connectDB() {
+  try {
+    await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connection successful");
+  } catch (error) {
+    console.error("Database connection error:", error);
+    process.exit(1);
+  }
+}
+
+connectDB();
+
+
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
-module.exports = app;
+
+
+// Logout route
+// app.get("/logout", (req, res, next) => {
+//   req.logout((err) => {
+//     if (err) {
+//       console.error("Error logging out:", err);
+//       return next(err);
+//     }
+//     res.redirect("/landingPage");
+//   });
+// });
+
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send("Internal Server Error");
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
